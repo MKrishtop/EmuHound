df5359054a5414001d85742c499db24d72351548
Mykhailo Kryshtop <mykhailokryshtop@MBP-MPTV2.local>
2018-03-01 10:10:39 +0200

    output results on sample screen

--- sample/src/main/kotlin/com/github/mkrishtop/emuhound/sample/SampleActivity.kt
+++ sample/src/main/kotlin/com/github/mkrishtop/emuhound/sample/SampleActivity.kt
@@ -2,10 +2,10 @@ package com.github.mkrishtop.emuhound.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.github.mkrishtop.emuhound.EmuHound
-import com.github.mkrishtop.emuhound.HoundOutputObserver
 import com.github.mkrishtop.emuhound.HoundOutput
+import com.github.mkrishtop.emuhound.HoundOutputObserver
+import kotlinx.android.synthetic.main.activity_sample.*
 
 class SampleActivity : AppCompatActivity() {
 
@@ -16,9 +16,13 @@ class SampleActivity : AppCompatActivity() {
         EmuHound.with(this.applicationContext
         ).addObserver(object: HoundOutputObserver {
             override fun onResult(output: HoundOutput) {
+                val outputText = StringBuilder()
+
                 output.triggeredRules.forEach {
-                    Log.d("SampleActivity", "Triggered rule: " + it)
+                    outputText.append("Triggered rule: " + it).append("\n")
                 }
+
+                outputTv.text = if (outputText.isEmpty()) "Not an emulator." else outputText.toString()
             }
         }).detectAsync()
     }
